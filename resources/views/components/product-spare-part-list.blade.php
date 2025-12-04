ce91ec2af702f17016c4fdbdc5413ef7e947b873
Fran Rey <franreycastedo@gmail.com>
2025-12-04 09:47:33 +0100

    add: aligned buttons

--- resources/views/components/product-spare-part-list.blade.php
+++ resources/views/components/product-spare-part-list.blade.php
@@ -1,9 +1,20 @@
 <div class="ml-3">
     <ul>
     @foreach ($relatedSpareparts as $spare_part)
-        <li class="p-1 flex gap-2 items-center">
-            <div class="flex-1 flex items-center text-left">⚙️ - {{ $spare_part->name }}</div>
-            <div class="flex items-center text-left font-bold">
+        <div class="grid grid-cols-[1fr_auto] md:grid-cols-[1fr_auto_auto] gap-1">
+            <div
+                class="p-2
+                    row-start-1
+                    md:col-auto md:row-auto
+                    flex items-center">
+                ⚙️ - {{ $spare_part->name }}
+            </div>
+
+            <div
+                class="p-2 font-bold
+                    row-start-2
+                    md:col-auto md:row-auto
+                    flex items-center">
                 @if($spare_part->price_with_discount)
                     <span class="text-green-600">
                         {{ $spare_part->getFormattedPriceWithDiscount() }}
@@ -16,8 +27,16 @@
                     {{ $spare_part->getFormattedPrice() }}
                 </span>
             </div>
-            @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
-        </li>
+
+            <div
+                class="p-2
+                    col-start-2 row-span-2
+                    flex items-center
+                    w-max
+                    md:col-auto md:row-span-1 md:w-auto md:flex-none">
+                @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
+            </div>
+        </div>
     @endforeach
     </ul>
 </div>
