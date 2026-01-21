358a8db4831ab722fcf9bb59890a583accd840ea
Fran Rey <franreycastedo@gmail.com>
2026-01-21 22:28:13 +0100

    fix: pint

--- app/Services/AddressBuilder.php
+++ app/Services/AddressBuilder.php
@@ -7,7 +7,6 @@ namespace App\Services;
 use App\Enums\AddressType;
 use App\Enums\PaymentMethod;
 use App\Enums\Role;
-use App\Events\UserCreated;
 use App\Models\Address;
 use App\Models\User;
 use Exception;
