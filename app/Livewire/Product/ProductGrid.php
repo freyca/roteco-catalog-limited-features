265f35d0988cec4613c63c6e26f869ed9b0a4b7c
Fran Rey <franreycastedo@gmail.com>
2026-02-03 22:52:59 +0100

    add: page does not has filters

--- app/Livewire/Product/ProductGrid.php
+++ app/Livewire/Product/ProductGrid.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace App\Livewire\Product;
 
 use App\Models\BaseProduct;
-use App\Repositories\Database\Product\BaseProductRepositoryInterface;
 use App\Repositories\Database\Product\Product\EloquentProductRepository;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\View\View;
-use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
@@ -19,71 +17,15 @@ class ProductGrid extends Component
     use WithoutUrlPagination, WithPagination;
 
     /**
-     * Used only for comparison, do not touch it
-     *
-     * @var array{'min_price': int, 'max_price': int, 'filtered_features': array<int>, 'filtered_category': int}
-     */
-    public array $default_filters = [
-        'filtered_category' => 0,
-        'min_price' => 0,
-        'max_price' => 10000,
-        'filtered_features' => [],
-    ];
-
-    /**
-     * @var array{'min_price': int, 'max_price': int, 'filtered_features': array<int>, 'filtered_category': int}
-     */
-    public array $filters = [
-        'filtered_category' => 0,
-        'min_price' => 0,
-        'max_price' => 10000,
-        'filtered_features' => [],
-    ];
-
-    public string $class_filter;
-
-    /**
      * @var LengthAwarePaginator<int, BaseProduct>
      */
     private LengthAwarePaginator $products;
 
-    public function mount(): void
-    {
-        $this->class_filter = EloquentProductRepository::class;
-    }
-
-    /**
-     * @param  array{'min_price': int, 'max_price': int, 'filtered_features': array<int>, 'filtered_category': int}  $filters
-     */
-    #[On('refreshProductGrid')]
-    public function getFilteredProducts(array $filters): void
-    {
-        // If no filters has been set, return all products
-        if ($filters === $this->default_filters) {
-            /** @var BaseProductRepositoryInterface<BaseProduct> $repository */
-            $repository = resolve($this->class_filter);
-
-            $this->products = $repository->getAll();
-        }
-
-        // If filters change, we reset url pagination and save them
-        // Need to save them so pagination does not breaks filters
-        if ($filters !== $this->filters) {
-            $this->filters = $filters;
-            $this->resetPage();
-        }
-
-        /** @var BaseProductRepositoryInterface<BaseProduct> $repository */
-        $repository = resolve($this->class_filter);
-
-        $this->products = $repository->getAll();
-    }
-
     public function render(): View
     {
-        if (! isset($this->products)) {
-            $this->getFilteredProducts($this->default_filters);
-        }
+        $repository = resolve(EloquentProductRepository::class);
+
+        $this->products = $repository->getAll();
 
         return view(
             'livewire.product.product-grid',
